615ae43a057c4b22c629e61e1e2b9b8b4b92ed6e
meekly1 <125712903+meekly1@users.noreply.github.com>
2024-07-12 16:47:31 +0800

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      <h1>JAY MIKE ALTERADO</h1>
+      <h2>AARON CANASA</h2>
+       
+    </div>
+  )
+}
+
+export default App
